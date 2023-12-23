0bd3af25595cc8fb7143306626902c279cda3d52
jeongwwon <spwldmlgla@naver.com>
2023-12-23 21:20:32 +0900

    사과

---
+++ 사과 담기.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int n, m, j,r,l,temp,ret;
+int main() {
+	cin >> n >> m >> j;
+	l = 1;
+	for (int i = 0; i < j; i++) {
+		cin >> temp;
+		r = l + m - 1;
+		if (temp >= l && temp <= r) continue;
+		else {
+			if (temp < l) {
+				ret += (l- temp);
+				l = temp;
+			}
+			else {
+				l += (temp - r);
+				ret += (temp - r);
+			}
+		}
+	}
+	cout << ret << "\n";
+	return 0;
+}
